4df3c2f0c48ab8fede3b4b3b896449c59a75de89
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-06 14:55:34 -0700

    Api - reconfigure users v1 api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users
+      resources :users, only: %i[index show update destroy]
     end
   end
 end
