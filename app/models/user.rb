72fd7228dfbdea2167ce402d500b83b9acecb9a0
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-07 12:10:20 -0700

    Api - add validation and secure password token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { in: 1..6 }
-  validates :password_confirmation, presence: true
+    has_secure_password
+
+    validates :username, presence: true
+    validates :password, presence: true, length: { in: 1..6 }
 end
