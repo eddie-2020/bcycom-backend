97972d66fc3cead68a5cb92a4f3502a92c13323b
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-07 12:45:53 -0700

    Api - authorize logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,12 @@ class ApplicationController < ActionController::API
     user_id = decoded_token[0]['user_id']
     @user = User.find_by(id: user_id)
   end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
