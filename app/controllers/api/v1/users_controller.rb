e74f70c44375ada12282914572f5f024e68fdcfd
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-07 12:49:13 -0700

    Api - re-generate user authentication token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      # token = encode_token({user_id: @user.id})
-      render json: { user: @user }
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }
     else
       render json: { error: 'Invalid username or password' }
     end
@@ -15,8 +15,8 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
 
     if @user&.authenticate(params[:password])
-      # token = encode_token({user_id: @user.id})
-      render json: { user: @user }
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: }
     else
       render json: { error: 'Invalid username or password' }
     end
