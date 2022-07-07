2f4745148d3bf9c7c7c427d3c933b37aabbb28e4
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-07 13:04:02 -0700

    Api - reconfigure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resource :users, only: %i[create]
+      resources :users, only: %i[create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
     end
   end
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
 end
