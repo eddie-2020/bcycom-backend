057be8890bfb52a4da3c48bc6c50bc95e7e73a23
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-11 23:46:04 -0700

    Api - validate motorcycle model

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,3 +1,10 @@
 class Motorcycle < ApplicationRecord
   belongs_to :user
+
+  validates :model, presence: true
+  validates :description, presence: true, length: { in: 1..100 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :image, presence: true
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :discount, presence: true
 end
