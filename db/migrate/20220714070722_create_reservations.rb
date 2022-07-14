063a7c8c73f2cbc4b0ed6e968dfc624696f7b516
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-14 00:11:27 -0700

    Api - migrate reservation db

--- db/migrate/20220714070722_create_reservations.rb
+++ db/migrate/20220714070722_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.references :user, null: false, foreign_key: { to_table: :users }, index:true
-      t.references :motorcycle, null: false, { to_table: :motorcycles }, index:true
+      t.references :motorcycle, null: false, foreign_key: { to_table: :motorcycles }, index:true
 
       t.timestamps
     end
