103595d60e4e55800e69798bd5c04f0679ac1048
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-14 00:24:15 -0700

    Api - add reservation params requirements

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -10,4 +10,10 @@ class Api::V1::ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
     render json: { reservation: @reservation }
   end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservation_time, :motorcycle_id)
+  end
 end
