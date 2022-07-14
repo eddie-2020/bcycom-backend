4eb8235bd47b1ba4d81b3c898120d602f089fa4d
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-14 00:13:44 -0700

    Api - add reservation index action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authorized, only: %i[index create destroy]
+
+  def index
+    @user_reservations = Reservation.where(user_id: params[:user_id])
+    render json: { user_reservations: @user_reservations }
+  end
+
 end
